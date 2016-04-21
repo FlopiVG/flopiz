796a5ca92bfa75bf178a26ea97f5fb51d5fc3ed6
Flopi <flopivillagarcia@gmail.com>
2016-04-21 14:05:41 +0200

    Create Actor and Player, player can move in client with ASDW

--- server.js
+++ server.js
@@ -13,7 +13,123 @@ app.use('/client', express.static(__dirname + '/client'));
 serv.listen(5000);
 console.log("Server started.");
 
+var Entity = function(id, x, y, width, height){
+    var self = {
+        id: id,
+        x: x,
+        y: y,
+        width: width,
+        height: height
+    };
+
+    return self;
+};
+var Actor = function (id, x, y, width, height) {
+    var self = Entity(id, x, y, width, height);
+    self.spdX = 0;
+    self.spdY = 0;
+    self.maxSpd = 10;
+
+    self.update = function(){
+        self.updatePosition();
+    };
+
+    self.updatePosition = function(){
+        self.x += self.spdX;
+        self.y += self.spdY;
+    };
+
+    return self;
+};
+var Player = function(id, x, y, width, height){
+    var self = Actor(id, x, y, width, height);
+    self.pressingRight = false;
+    self.pressingLeft = false;
+    self.pressingUp = false;
+    self.pressingDown = false;
+
+    var super_update = self.update;
+    self.update = function(){
+        super_update();
+        self.updateSpd();
+    };
+
+    self.updateSpd = function(){
+        if(self.pressingRight) self.spdX = self.maxSpd;
+        else if (self.pressingLeft) self.spdX = -self.maxSpd;
+        else self.spdX = 0;
+
+        if(self.pressingUp) self.spdY = -self.maxSpd;
+        else if(self.pressingDown) self.spdY = self.maxSpd;
+        else self.spdY = 0;
+    };
+
+    Player.list[id] = self;
+    return self;
+};
+Player.list = {};
+Player.onConnect = function(socket){
+    var player = Player(socket.id, 100, 100, 20, 20);
+
+    socket.on('keyPress', function(data){
+        if(data.inputId === 'left')
+            player.pressingLeft = data.state;
+        else if(data.inputId === 'right')
+            player.pressingRight = data.state;
+        else if(data.inputId === 'up')
+            player.pressingUp = data.state;
+        else if(data.inputId === 'down')
+            player.pressingDown = data.state;
+        else if(data.inputId === 'attack')
+            player.pressingAttack = data.state;
+        else if(data.inputId === 'mouseAngle')
+            player.mouseAngle = data.state;
+    });
+};
+Player.onDisconnect = function(socket){
+    delete Player.list[socket.id];
+};
+Player.update = function(socket){
+    var pack = [];
+    for (var i in Player.list){
+        var player = Player.list[i];
+        player.update();
+        pack.push({
+            x: player.x,
+            y: player.y,
+            width: player.width,
+            height: player.height
+        });
+    }
+    return pack;
+};
+
+var SOCKET_LIST = {};
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
     console.log("Client conected");
+    socket.id = Math.random();
+    SOCKET_LIST[socket.id] = socket;
+    Player.onConnect(socket);
+
+    socket.on("disconnected", function () {
+        delete SOCKET_LIST[socket.id];
+        Player.onDisconnect(socket);
+    });
+
+    socket.on('latency', function (startTime, cb) {
+        cb(startTime);
+    });
 });
+
+// GAME LOOP
+setInterval(function(){
+    var pack = {
+        player: Player.update()
+    };
+
+    for (var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.emit("newPosition",pack);
+    }
+},1000/25);
